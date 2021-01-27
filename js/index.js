267c9ef1b44c7a34ae4d1035b0851f74c97acf69
John Vasile <johny_tnt@yahoo.com>
2021-01-27 13:09:45 -0500

    Iteration 1 - Draw the game board

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 window.onload = () => {
+  document.getElementById('game-board' ).style.backgroundImage = "url('./images/road.png')";
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
   function startGame() {}
 };
+
+
+
+// let canvas = document.querySelector('canvas');
+
+// canvas.width =window.innerWidth;
+// canvas.height=window.innerHeight;
+
+// let ctx = canvas.getContext('2d');
+
+// //    x,   y,   w,    h
+
+// ctx.fillStyle = "green";
+// ctx.fillRect(100, 0, 50, 200)
